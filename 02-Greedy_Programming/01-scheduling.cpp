de93254d6051bf9499664c9811c40481370d5831
enxel <26006596+enxel@users.noreply.github.com>
2026-05-14 10:15:52 -0600

    Add Greedy example

---
+++ 02-Greedy_Programming/01-scheduling.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<pair<int,int>> jobs = { {86, 78}, {25, 26}, {88, 54}, {16, 45}, {24, 76} };
+
+    sort(jobs.begin(), jobs.end(), 
+        [](pair<int,int> a, pair<int,int> b) {
+            return a.second/a.first > b.second/b.first;
+        }
+    );
+
+    cout << "La agenda es:\n";
+    for(pair<int,int> p: jobs) {
+        cout << p.first << " - " << p.second << "\n";
+    }
+
+    return 0;
+}
